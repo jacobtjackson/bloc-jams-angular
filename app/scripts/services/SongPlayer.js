81d88aa3dcc573e59710b2496d6bd6d716385677
Jacob Jackson <jacob.t.jackson@gmail.com>
2016-06-01 15:27:30 -0500

    create PlayerBarCtrl and add play, pause, and next buttons

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,10 +1,28 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
         
-        var currentSong = null;
+        /**
+        *@function currentAlbum
+        *@desc gets album information from the fixtures service
+        *@param none
+        */
+        var currentAlbum = Fixtures.getAlbum();
         
+        /**
+        *@desc gets the index of a song from the album
+        *@param {object} song
+        *@returns index of song
+        */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        }
         
+        /**
+        * @desc Active song object from list of songs
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
         var currentBuzzObject = null;
         
         /**
@@ -13,7 +31,6 @@
         *@param {object} song
         */
         var playSong = function(song) {
-            setSong(song);
             currentBuzzObject.play();
             song.playing = true;
         };
@@ -26,7 +43,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
             
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -34,23 +51,45 @@
                 preload: true
             });
             
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
         
         SongPlayer.play = function(song) {
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
+                setSong(song);
                 playSong(song);
-            } else if (currentSong === song) {
+            } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                   playSong(song);
                 }
             }
         };
         
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
         };
+        
+        /**
+        *@desc sets the currentSong to the previous song in the album
+        *@param {object} song
+        */
+        SongPlayer.previous = function(song) {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+            
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
         return SongPlayer;
     }
     
